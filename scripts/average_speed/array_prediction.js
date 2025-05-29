b93db5cfd5286f760481a503e6c39f7a06bae461
CJSadowitz <sadowitzc@gmail.com>
2025-05-29 12:33:21 -0400

    integral for solar angle

--- scripts/average_speed/array_prediction.js
+++ scripts/average_speed/array_prediction.js
@@ -1,8 +1,9 @@
-import {calculate_solar_angle} from "../sun_calculations/solar_altitude.js";
+import {calculate_solar_angle, integrate_solar_angle} from "../sun_calculations/solar_altitude.js";
 import {array_power_coefficient} from "../../constants/array.js"
 
 function array_power(time) {
-    return array_power_coefficient * Math.cos(calculate_solar_angle(time).altitude);
+    console.log(integrate_solar_angle(new Date("2025-05-29T10:00:00"), new Date("2025-05-29T18:00:00")))
+    return array_power_coefficient * Math.cos(calculate_solar_angle(time));
 }
 
 window.array_power = array_power;
--- scripts/sun_calculations/solar_altitude.js
+++ scripts/sun_calculations/solar_altitude.js
@@ -1,7 +1,20 @@
 import { latitude, longitude } from "../../constants/location.js";
 
 export function calculate_solar_angle(time=new Date()) {
-    return SunCalc.getPosition(time, latitude, longitude);
+    return SunCalc.getPosition(time, latitude, longitude).altitude;
+}
+
+// adapt to work for other days
+export function integrate_solar_angle(start_time, end_time) {
+    let sum = 0;
+    let step_minutes = 1;
+    
+    for (let time = new Date(start_time); time <= end_time; time.setMinutes(time.getMinutes() + step_minutes)) {
+        let slice = calculate_solar_angle(time) * step_minutes; 
+        sum += Math.cos(slice);
+    }
+
+    return sum;
 }
 
 export function calculate_high_noon_angle() {
