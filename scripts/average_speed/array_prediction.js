6f70bb1f765bb780f78825f8dc3d6024e0c33be7
CJSadowitz <sadowitzc@gmail.com>
2025-06-04 12:02:58 -0400

    Accurate constants, error checking for invalid date

--- scripts/average_speed/array_prediction.js
+++ scripts/average_speed/array_prediction.js
@@ -6,8 +6,12 @@ function array_power(time) {
     return array_power_coefficient * Math.cos(calculate_solar_angle(time));
 }
 
-function array_power_t_to_t(start, end) {
-    return solar_irradiance * integrate_solar_angle(start, end) * array_area * array_efficiency;
+export function array_power_t_to_t(start, end) {
+    if (start instanceof Date && !isNaN(start) && end instanceof Date && !isNaN(end)) {
+        return solar_irradiance * integrate_solar_angle(start, end) * array_area * array_efficiency;
+    }
+    console.error("ARRAY_PREDICTION::ARRAY_POWER_T_TO_T::invalid date", start, end);
+    return -1;
 }
 
 window.array_power_t_to_t = array_power_t_to_t;
