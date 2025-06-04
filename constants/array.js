86cf39f4ca9de0f90b609a6cc0bdf8c8cd3aab1c
CJSadowitz <sadowitzc@gmail.com>
2025-06-04 11:48:51 -0400

    function to get integrated sun angle

--- constants/array.js
+++ constants/array.js
@@ -1,3 +1,5 @@
 export const array_voltage = 6; // temp
-export const array_max_power = 960; // temp
+export const array_max_power = 975.96;
 export const array_power_coefficient = 100; // temp
+export const array_efficiency = 0.24;
+export const array_area = 4;
--- scripts/average_speed/array_prediction.js
+++ scripts/average_speed/array_prediction.js
@@ -1,9 +1,14 @@
 import {calculate_solar_angle, integrate_solar_angle} from "../sun_calculations/solar_altitude.js";
-import {array_power_coefficient} from "../../constants/array.js"
+import {array_power_coefficient, array_efficiency, array_area} from "../../constants/array.js"
+import {solar_irradiance} from "../../constants/sun.js";
 
 function array_power(time) {
-    console.log(integrate_solar_angle(new Date("2025-05-29T10:00:00"), new Date("2025-05-29T18:00:00")))
     return array_power_coefficient * Math.cos(calculate_solar_angle(time));
 }
 
+function array_power_t_to_t(start, end) {
+    return solar_irradiance * integrate_solar_angle(start, end) * array_area * array_efficiency;
+}
+
+window.array_power_t_to_t = array_power_t_to_t;
 window.array_power = array_power;
